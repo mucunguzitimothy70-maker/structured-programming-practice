aaaa16ce762e008fff59ab7f640bb9be2e1570a6
mucunguzitimothy70-maker <mucunguzitimothy70@gmail.com>
2026-09-25 15:54:40 +0300

    Update exercise.c

--- 03_decisions/exercise.c
+++ 03_decisions/exercise.c
@@ -1,1 +1,18 @@
+//Exercise 2.22
+#include <stdio.h>
+int main()
+{
+    int integer;
+    printf("ENTER AN INTEGER\n");
+    scanf("%d", &integer); 
+    
+    if (integer % 2 == 0){
+        printf("even\n");
+    }
+    else{
+        printf("odd \n");
+    }
+    
+    return 0;
+}
 
