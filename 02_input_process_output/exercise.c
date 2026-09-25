6ccd10254744c520bfab99e2af7828d76d5ce69f
mucunguzitimothy70-maker <mucunguzitimothy70@gmail.com>
2026-09-25 15:56:22 +0300

    Update exercise.c

--- 02_input_process_output/exercise.c
+++ 02_input_process_output/exercise.c
@@ -1,18 +1,23 @@
-//Exercise 2.22
+//Exercise 2.16
 #include <stdio.h>
 int main()
 {
-    int integer;
-    printf("ENTER AN INTEGER\n");
-    scanf("%d", &integer); 
+    int num1, num2, sum, prod, diff ,quotient, remainder;
     
-    if (integer % 2 == 0){
-        printf("even\n");
-    }
-    else{
-        printf("odd \n");
-    }
+    printf("ENTER  INTEGER 1\n");
+    scanf("%d", &num1); 
     
+    printf("ENTER  INTEGER 1\n");
+    scanf("%d", &num2); 
+    
+    
+  sum=num1+ num2;
+  prod=num1*num2;
+  diff=num1-num2;
+  quotient=num1/num2;
+  remainder=num1%num2;
+  
+  printf("Sum:%d \n Product:%d \n Difference: %d \n Quotient: %d \n Remainder:%d",sum,  prod,diff,quotient, remainder);
     return 0;
 }
 
