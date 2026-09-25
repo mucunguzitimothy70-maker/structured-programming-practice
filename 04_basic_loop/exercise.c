f3d1d5c03df829b1bd74d6e2e19849b278849369
mucunguzitimothy70-maker <mucunguzitimothy70@gmail.com>
2026-09-25 15:57:37 +0300

    Update exercise.c

--- 04_basic_loop/exercise.c
+++ 04_basic_loop/exercise.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    long sum = 0;
+    int count = 0;
+
+    
+    for (int i = 1; i <= 100; i++) {
+        // Check if i is a multiple of 7
+        if (i % 7 == 0) {
+            sum += i;
+            count++;
+        }
+    }
+
+    printf("--- Multiples of 7 from 1 to 100 ---\n");
+    printf("Total multiples found: %d\n", count);
+    printf("Sum of all multiples:  %lld\n", sum);
+
+    return 0;
+}
 
